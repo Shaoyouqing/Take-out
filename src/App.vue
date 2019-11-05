72d9cca0914651f3e92ead0245628440e04578af
shaoyouqing <shaoyouqing@imagicdatatech.com>
2019-11-05 10:36:35 +0800

    test2

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template class="app">
+  <div>
+    <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+  </div>
+</template>
+<script>
+import FooterGuide from "./components/FooterGuide/FooterGuide"
+export default {
+ name:'App',
+ components:{
+   FooterGuide
+ },
+ data() {
+   return {
+     
+   }
+ },
+}
+</script>
+<style lang="stylus" ref="stylesheet/stylus">
+    .app
+      width 100%
+      height 100%
+      background #f5f5f5
+</style>
